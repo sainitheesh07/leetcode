800e6afa43fab00016611d857b5467d607d0aee2
G N V SAI NITHEESH <gnvsainitheesh@gmail.com>
2026-06-01 15:42:18 +0530

    Time: 0 ms (100%), Space: 42.1 MB (40.13%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.java
+++ 2427-number-of-common-factors/2427-number-of-common-factors.java
@@ -1,12 +1,8 @@
 class Solution {
     public int commonFactors(int a, int b) {
-        HashSet<Integer> hs=new HashSet<>();
-        for(int i=1;i<=a;i++){
-            if(a%i==0) hs.add(i);
-        }
-        int c=0;
-        for(int i=1;i<=b;i++){
-            if(b%i==0 && hs.contains(i)) c++;
+        int c=0,min=Math.min(a,b);
+        for(int i=1;i<=min;i++){
+            if(a%i==0 && b%i==0) c++;
         }
         return c;
     }
