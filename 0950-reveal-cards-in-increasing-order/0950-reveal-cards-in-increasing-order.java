c01c18a0bb1f10e23340c5f0de3afb4a4143e78c
G N V SAI NITHEESH <gnvsainitheesh@gmail.com>
2026-06-06 14:00:07 +0530

    Time: 7 ms (93.96%), Space: 45.7 MB (57%) - LeetHub

---
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] deckRevealedIncreasing(int[] deck) {
+        Arrays.sort(deck);
+        int n=deck.length;
+        Deque<Integer> st=new ArrayDeque<>();
+        st.addFirst(deck[n-1]);
+        for(int i=n-2;i>=0;i--){
+            st.addFirst(st.removeLast());
+            st.addFirst(deck[i]);
+        }
+        for(int i=0;i<n;i++){
+            deck[i]=st.removeFirst();
+        }
+        return deck;
+    }
+}
