456afa01257964ef1f565ed6405aaa6433ebe6c6
G N V SAI NITHEESH <gnvsainitheesh@gmail.com>
2026-06-06 15:21:28 +0530

    Time: 2 ms (98.73%), Space: 47.3 MB (25.32%) - LeetHub

---
+++ 0942-di-string-match/0942-di-string-match.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] diStringMatch(String s) {
+       int n=s.length();
+       int[] res=new int[n+1];
+       int low=0,high=n;
+       for(int i=0;i<n;i++){
+        if(s.charAt(i)=='I') res[i]=low++;
+        else res[i]=high--;
+       }
+       res[n]=low;
+       return res;
+    }
+}
