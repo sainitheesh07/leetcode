dadfd14644c73a6eb77e8874893d483a5a6f8a08
G N V SAI NITHEESH <gnvsainitheesh@gmail.com>
2026-06-09 21:46:59 +0530

    Time: 1 ms (99.29%), Space: 42.7 MB (32.11%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.java
+++ 0263-ugly-number/0263-ugly-number.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isUgly(int n) {
-        for(int i=2;i<6 && n>0;i++){
-            while(n%i==0){
-                n/=i;
-            }
-        }
+        if(n<=0) return false;
+        while(n%2==0) n/=2;
+        while(n%3==0) n/=3;
+        while(n%5==0) n/=5;
         return n==1;
     }
 }
