f44254b99bf39af6485c37c17d0b44ede425af59
G N V SAI NITHEESH <gnvsainitheesh@gmail.com>
2026-06-16 23:35:16 +0530

    Time: 4 ms (76.47%), Space: 55.1 MB (75%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(c=='%') sb.reverse();
+            else if(c=='*'){
+                if(sb.length()!=0){
+                    sb.deleteCharAt(sb.length()-1);
+                }
+            }
+            else if(c=='#') sb.append(sb);
+            else if(Character.isLetter(c)) sb.append(c);
+        }
+        return sb.toString();
+    }
+}
